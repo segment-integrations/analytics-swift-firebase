583093ff81e57894c02f085580981c6606d87c6c
Alan Charles <acharles@twilio.com>
2023-01-27 17:50:31 -0700

    fix example and bump dependencies

--- Package.swift
+++ Package.swift
@@ -23,12 +23,12 @@ let package = Package(
         .package(
             name: "Segment",
             url: "https://github.com/segmentio/analytics-swift.git",
-            from: "1.1.2"
+            from: "1.3.4"
         ),
 		.package(
 			name: "Firebase",
 			url: "https://github.com/firebase/firebase-ios-sdk",
-			from: "9.1.0"
+			from: "10.0.0"
 		)
     ],
     targets: [
