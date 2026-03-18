e75b995887c8c9ee400c058b75e6028cb0108de1
Syed Sadrul Ullah Sahad <syed.sahad@mindvalley.com>
2026-03-18 15:39:45 +0800

    Bump minimum iOS platform to 15.0 for Firebase 12 compatibility

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@ let package = Package(
     name: "SegmentFirebase",
     platforms: [
         .macOS("10.15"),
-        .iOS("13.0"),
-        .tvOS("13.0"),
+        .iOS("15.0"),
+        .tvOS("15.0"),
         .watchOS("7.1")
     ],
     products: [
